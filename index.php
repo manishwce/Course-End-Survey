232777d37ce8872c9bd41bdc58063963a7fdacce
manishwce <manishwce@gmail.com>
2016-09-07 16:28:23 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php include 'includes/overall/overallheader.php'; ?>
+                    
+                    <!-- === BEGIN CONTENT === -->
+				<div id="content">
+                        
+                        <div class="container background-gray-lighter">
+                            <div class="row">
+                                <h2 class="animate fadeIn text-center margin-top-50">Welcome to Course End Survey Portal</h2>
+                                <hr class="margin-top-15">
+                                <br><p class="animate fadeIn text-center">Here you can complete Course End Survey for all the courses you learnt!</p>
+                            <!--<p class="text-center animate fadeInUp margin-bottom-50">
+                                    <button type="button" class="btn btn-lg btn-primary">View Details</button>
+                                </p> -->
+								
+                            </div>
+                        </div>
+                        <div class="container">
+                            <div class="row margin-vert-30">
+                                <!-- Main Text -->
+                                <div class="col-md-9">
+                                    <center><h3>Online Course End Survey Portal for CE & IT Department students</h3></center><br>
+                                    <p>This Online Course End Survey Portal is useful for the following Programs only-</p>
+										<ul>
+											<li>B.Tech.Computer Engineering</li>
+											<li>B.Tech.Information Technology</li>
+											<li>M.Tech.Computer Engineering</li>
+											<li>M.Tech.Software Engineering</li>
+											<li>M.Tech.NIMS (Network Infrastructure Management System)</li>
+										</ul>
+										<div class="clearfix:after"></div>
+                                </div>
+                                <!-- End Main Text -->
+                                <!-- Side Column -->
+                                <div class="col-md-3">
+                                    <h3 class="title">Opening hours</h3>
+                                    <h6 style="margin: 0;">Mon - Wed</h6>
+                                    <h4 style="margin: 0;">7:00 am - 1:30 pm</h4>
+                                    <p>
+                                        <small>* Lorem ipsum dolor sit amet</small>
+                                    </p>
+                                    <hr>
+                                    <h6 style="margin: 0;">Thursday</h6>
+                                    <h4 style="margin: 0;">7:00 am - 1:30 pm</h4>
+                                    <p>
+                                        <small>* Lorem ipsum dolor sit amet</small>
+                                    </p>
+                                    <hr>
+                                </div>
+                                <!-- End Side Column -->
+                            </div>
+                        </div>
+					 
+                    
+				</div>
+
+
+					<?php include 'includes/overall/overallfooter.php'; ?>
